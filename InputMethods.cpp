a9fc30952f55af3861ecd7f0084b9a7747d8ab65
Mateusz Wiktorowicz <wktorowicz.mat@gmail.com>
2023-10-30 11:41:52 +0100

    add readSign, readNum, readLine functions

---
+++ InputMethods.cpp
@@ -0,0 +1,44 @@
+#include "InputMethods.h"
+
+string InputMethods::readLine()
+{
+    string input = "";
+    getline(cin, input);
+    return input;
+}
+
+char InputMethods::readSign()
+{
+    string input = "";
+    char sign  = {0};
+
+    while (true)
+    {
+        getline(cin, input);
+
+        if (input.length() == 1)
+        {
+            sign = input[0];
+            break;
+        }
+        cout << "To nie jest pojedynczy znak. Wpisz ponownie." << endl;
+    }
+    return sign;
+}
+
+int InputMethods::readNumber()
+{
+    string input = "";
+    int num = 0;
+
+    while (true)
+    {
+        getline(cin, input);
+
+        stringstream myStream(input);
+        if (myStream >> num)
+            break;
+        cout << "To nie jest liczba. Wpisz ponownie. " << endl;
+    }
+    return num;
+}
