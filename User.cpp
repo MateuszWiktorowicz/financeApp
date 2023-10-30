a66ab37182aabce4be17d7028124d544e4d5e3da
Mateusz Wiktorowicz <wktorowicz.mat@gmail.com>
2023-10-30 10:41:31 +0100

    add attributes and setters to User class

---
+++ User.cpp
@@ -0,0 +1,22 @@
+#include "User.h"
+
+void User::setId(int i)
+{
+    id = i;
+}
+void User::setLogin(string log)
+{
+    login = log;
+}
+void User::setPassword(string pass)
+{
+    password = pass;
+}
+void User::setName(string nam)
+{
+    name = nam;
+}
+void User::setSurname(string surnam)
+{
+    surname = surnam;
+}
