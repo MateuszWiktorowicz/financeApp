4a4fab909235b7f318188b8c961a1e30c9e72fb9
Mateusz Wiktorowicz <wktorowicz.mat@gmail.com>
2023-10-31 11:55:29 +0100

    add isFileEmpty(), and <fstream> header

--- XmlFile.cpp
+++ XmlFile.cpp
@@ -4,3 +4,12 @@ string XmlFile::getFileName()
 {
     return FILE_NAME;
 }
+
+bool XmlFile::isFileEmpty(fstream &textFile)
+{
+    textFile.seekg(0, ios::end);
+    if (textFile.tellg() == 0)
+        return true;
+    else
+        return false;
+}
--- XmlFile.h
+++ XmlFile.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "Markup.h"
 
@@ -11,4 +12,5 @@ class XmlFile
 public:
     XmlFile(string fileName) : FILE_NAME(fileName) {};
     string getFileName();
+    bool isFileEmpty(fstream &textFile);
 };
